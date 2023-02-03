fba11acdf899c9512754d704d99d5fc91d7a06b2
antos07 <trotsenko.ant07@gmail.com>
2023-02-03 22:19:35 +0100

    Add string representation for models

--- website/recipes/models.py
+++ website/recipes/models.py
@@ -8,10 +8,16 @@ class Meal(models.Model):
     image_url = models.URLField(max_length=200, null=True, blank=True)
     video_instructions_url = models.URLField(max_length=200, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class IngredientCategory(models.Model):
     """Represents an ingredient category"""
     name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -20,7 +26,10 @@ class Ingredient(models.Model):
     description = models.TextField(null=True, blank=True)
     image_url = models.URLField(max_length=200, null=True, blank=True)
     meals = models.ManyToManyField(Meal, through='MealIngredientMeasure')
-    category = models.ForeignKey(IngredientCategory, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class MealIngredientMeasure(models.Model):
@@ -34,3 +43,6 @@ class MealIngredientMeasure(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['meal', 'ingredient'], name='unique_meal_ingredient_measure')
         ]
+
+    def __str__(self):
+        return f'"{self.ingredient}" in "{self.meal}": {self.measure}'
