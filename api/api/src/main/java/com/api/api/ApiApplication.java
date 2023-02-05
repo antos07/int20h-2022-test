a553d4d4237b6f906b06e84e80c76d1f66f94682
Polina Holotsvan <swan.polina00@gmail.com>
2023-02-05 15:01:58 +0200

    fridge added

--- api/api/src/main/java/com/api/api/ApiApplication.java
+++ api/api/src/main/java/com/api/api/ApiApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiApplication {
     User user=new User();
     Bucket bucket=new Bucket();
+    Fridge fridge=new Fridge();
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
@@ -22,7 +23,7 @@ public class ApiApplication {
     public String allMeals(){
         return bucket.allMeals();
     }
-    @GetMapping("isBucketEmpty")
+    @GetMapping("/isBucketEmpty")
     public boolean isBucketEmpty(){
       return bucket.isEmpty();
     }
