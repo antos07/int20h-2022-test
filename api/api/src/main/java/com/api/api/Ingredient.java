1d8c7d07ecda540b31f607ee5dbaba60a009613d
Polina Holotsvan <swan.polina00@gmail.com>
2023-02-06 14:38:30 +0200

    ingredients edited

---
+++ api/api/src/main/java/com/api/api/Ingredient.java
@@ -0,0 +1,70 @@
+package com.api.api;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private String description;
+    private int category_id;//??
+    private String image_url;
+    private double measure;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(int id, String name, String description, int category_id, String image_url, double measure) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category_id = category_id;
+        this.image_url = image_url;
+        this.measure = measure;
+    }
+
+    public double getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(double measure) {
+        this.measure = measure;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
