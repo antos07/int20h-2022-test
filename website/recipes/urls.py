4de58aa7a5d9bbf7ddfac8dfeca63eebae853076
antos07 <trotsenko.ant07@gmail.com>
2023-02-06 19:09:16 +0100

    Rename views

--- website/recipes/urls.py
+++ website/recipes/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('recipes/', views.MealListView.as_view(), name='index'),
-    path('recipes/<int:pk>/', views.MealDetailView.as_view(), name='detail'),
+    path('recipes/', views.MealListView.as_view(), name='meal_list'),
+    path('recipes/<int:pk>/', views.MealDetailView.as_view(), name='meal_details'),
     path('fridge/', views.IngredientListView.as_view(), name='fridge'),
 ]
