cf662cbcef215d2916d438bcb8347a88477d9c06
antos07 <trotsenko.ant07@gmail.com>
2023-02-06 15:48:16 +0100

    Add measures for ingredients in meal_detail.html

--- website/recipes/views.py
+++ website/recipes/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 
-from .models import Meal
+from .models import Meal, MealIngredientMeasure
 
 
 class MealListView(ListView):
@@ -9,3 +9,11 @@ class MealListView(ListView):
 
 class MealDetailView(DetailView):
     model = Meal
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Adding MealIngredientMeasure list for the meal
+        context['measures'] = MealIngredientMeasure.objects.filter(meal=context['meal'])
+
+        return context
