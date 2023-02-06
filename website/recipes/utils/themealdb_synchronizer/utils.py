f40ab8ece5e6414721503c5a0033eeee63ea1a9a
antos07 <trotsenko.ant07@gmail.com>
2023-02-06 20:11:12 +0100

    Fix building of ingredient urls

--- website/recipes/utils/themealdb_synchronizer/utils.py
+++ website/recipes/utils/themealdb_synchronizer/utils.py
@@ -4,10 +4,10 @@ import html
 def ingredient_image_url_by_name(ingredient_name: str) -> str:
     """Builds ingredient image url by its name"""
     ingredient_name = html.escape(ingredient_name)
-    return f'www.themealdb.com/images/ingredients/{ingredient_name}.png'
+    return f'https://www.themealdb.com/images/ingredients/{ingredient_name}.png'
 
 
 def ingredient_small_image_url_by_name(ingredient_name: str) -> str:
     """Builds ingredient image url by its name"""
     ingredient_name = html.escape(ingredient_name)
-    return f'www.themealdb.com/images/ingredients/{ingredient_name}-Small.png'
+    return f'https://www.themealdb.com/images/ingredients/{ingredient_name}-Small.png'
